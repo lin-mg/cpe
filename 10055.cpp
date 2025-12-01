c2dfc706ccaa228c9a2dea1f6c8b9c7bcf41b927
lin-mg <moon20040211@gmail.com>
2025-12-01 21:22:10 +0800

    Create 10055.cpp

---
+++ 10055.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    
+    while(cin >> a >> b){
+        cout << abs(b - a) << endl;
+    }
+
+    return 0;
+}
