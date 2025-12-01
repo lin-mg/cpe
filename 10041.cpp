85c53ec75a2c54de37319b64f6b46a9ee66adeff
lin-mg <moon20040211@gmail.com>
2025-12-01 21:02:02 +0800

    Create 10041.cpp

---
+++ 10041.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int r;
+        cin >> r;
+        
+        int s[r] = {};
+        for(int i = 0; i < r; i++){
+            cin >> s[i];
+        }
+        
+        int m = 99999999;
+        for(int i = 0; i < r; i++){
+            int sum = 0;
+            for(int j = 0; j < r; j++){
+                sum += abs(s[i] - s[j]);
+            }
+            m = min(m, sum);
+        }
+        cout << m << endl;
+    }
+
+    return 0;
+}
