7170e350fe835698302ff623ba2936b58d969ba6
lin-mg <moon20040211@gmail.com>
2025-12-02 02:06:10 +0800

    Update and rename 100.cpp to 100 - The 3n + 1 problem.cpp

--- 100 - The 3n + 1 problem.cpp
+++ 100 - The 3n + 1 problem.cpp
@@ -5,18 +5,22 @@ using namespace std;
 int main()
 {
     long long a, b;
-    
+
     while(cin >> a >> b){
+        // 先輸出再 swap 就不會有順序錯亂的問題
         cout << a << " " << b << " ";
+
+        // 敘述沒說 a 一定小於 b
         if(a > b){
             swap(a, b);
         }
-        
+
         int m = 0;
         for(int i = a; i <= b; i++){
             int n = i;
             int cnt = 0;
-            
+
+            // 題目給的虛擬碼
             while(1){
                 cnt++;
                 if (n == 1) {
