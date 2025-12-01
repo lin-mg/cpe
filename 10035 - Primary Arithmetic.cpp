2d912ec00cb1b5477ffee145b822da534ecf8806
lin-mg <moon20040211@gmail.com>
2025-12-02 02:13:15 +0800

    Update and rename 10035.cpp to 10035 - Primary Arithmetic.cpp

--- 10035 - Primary Arithmetic.cpp
+++ 10035 - Primary Arithmetic.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main()
 {
+    // 大小範圍超出 int
     long long a, b;
     
     while(cin >> a >> b){
+        // 可能會有一方測資為 0
         if(a == 0 && b == 0) {
             break;  
         }
+
         long long cnt = 0;
         int carry = 0;
-        
+
+        // 可能一方為 0 了，但另一個測資 + carry >= 10，所以不能 "a && b"
         while(a || b){
             if(((a%10) + (b%10) + carry) >= 10){
                 cnt++;
@@ -25,7 +29,8 @@ int main()
                 b /= 10;
             }
         }
-        
+
+        // 輸出小細節，複數 s
         if(cnt == 0){
             cout << "No carry operation." << endl;
         } else if(cnt == 1){
