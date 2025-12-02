b57c3dead95fc53f4752a1bcff7931ea4035b93d
lin-mg <moon20040211@gmail.com>
2025-12-02 21:05:08 +0800

    Update 10222 - Decode the Mad man.cpp

--- 10222 - Decode the Mad man.cpp
+++ 10222 - Decode the Mad man.cpp
@@ -1,3 +1,10 @@
+/*
+1. cin 不會讀空白字元，getchar 才會，因此這題要用 getchar 處理
+2. getchar 有返回值，讀到 EOF 會返回 -1
+3. c = tolower(c);    //轉小寫
+
+4. 題目沒有說不會換行，因此要補換行字元
+*/
 #include <bits/stdc++.h>
 
 using namespace std;
