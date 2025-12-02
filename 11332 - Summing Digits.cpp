cdc1f82da44ed3c7092552ca2ec3205b3eecbddd
lin-mg <moon20040211@gmail.com>
2025-12-02 18:47:30 +0800

    Create 11332 - Summing Digits.cpp

---
+++ 11332 - Summing Digits.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    
+    while((cin >> n) && n){
+        while(n >= 10){
+            int sum = 0;
+            while(n){
+                sum += n%10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        cout << n << endl;
+    }
+    
+    return 0;
+}
