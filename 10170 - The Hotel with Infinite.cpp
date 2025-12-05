2481778639e30db07451141932c05b24baaa956b
lin-mg <moon20040211@gmail.com>
2025-12-05 22:39:30 +0800

    Create 10170 - The Hotel with Infinite.cpp

---
+++ 10170 - The Hotel with Infinite.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    long long s, d;
+    
+    while(cin >> s >> d){
+        while(d > 0){
+            d -= s;
+            s++;
+        }
+        cout << s - 1 << endl;
+    }
+    
+    return 0;
+}
